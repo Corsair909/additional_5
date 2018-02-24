05df2a5b8d5d01c73d6026887b983ea5e194665b
Corsair909 <corsar909@gmail.com>
2018-02-24 22:29:55 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var step = 0;
+
+  var arr = str.split('');
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === '{' || arr[i] === '[' || arr[i] === '(') {
+      step++;
+    } else {
+      step--;
+    }
+    
+  }
+
+  if (step === 0) {
+    return true;
+  } else {
+    return false;
+  }
+
+
 }
